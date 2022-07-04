a49de28ca15de36cd79a3c15f5682ca821ba7c73
John <jrborges52@email.com>
2022-07-04 03:13:07 +0000

    add the form page

---
+++ public/scripts/jquery-styles.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+  let numberOfOptions = 2
+
+  $(`#add-option`).on("click", (event) =>{
+    event.preventDefault();
+    const $option = createAnotherOption()
+    numberOfOptions += 1;
+    $(`#add-option-div`).append($option);
+
+
+  })
+
+
+
+
+  const createAnotherOption = function() {
+
+    const $options = $(`
+
+    <input type="text" placeholder="Option # ${numberOfOptions}" class="option">
+    <textarea name="description" class="description-class" id="description" cols="30" rows="2" placeholder="Description of your option"></textarea>
+   `)
+
+    return $options
+  }
+
+
+
+
+
+
+})
