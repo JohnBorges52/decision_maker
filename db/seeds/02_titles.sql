489a6915bad3d398cb830b4b6771ad9e4895c61b
Zuge Gao <audreygaozu@hotmail.com>
2022-07-04 04:44:52 +0000

    add key values to user table,and add user_id for other table

--- db/seeds/02_titles.sql
+++ db/seeds/02_titles.sql
@@ -1,1 +1,2 @@
 INSERT INTO titles (title,user_id) VALUES ('what food you would like for dinner?',1);
+INSERT INTO titles (title,user_id) VALUES ('which movie you want to watch?',2);
--- db/seeds/03_options.sql
+++ db/seeds/03_options.sql
@@ -1,3 +1,8 @@
-INSERT INTO options (title_id,choice) VALUES (1,'pizza');
-INSERT INTO options (title_id,choice) VALUES (1,'hotpot');
-INSERT INTO options (title_id,choice) VALUES (1,'BBQ');
+INSERT INTO options (user_id,title_id,choice) VALUES (1,1,'pizza');
+INSERT INTO options (user_id,title_id,choice) VALUES (1,1,'hotpot');
+INSERT INTO options (user_id,title_id,choice) VALUES (1,1,'BBQ');
+INSERT INTO options (user_id,title_id,choice) VALUES (1,1,'KFC');
+INSERT INTO options (user_id,title_id,choice) VALUES (2,2,'Harry Potter');
+INSERT INTO options (user_id,title_id,choice) VALUES (2,2,'Iron Man');
+INSERT INTO options (user_id,title_id,choice) VALUES (2,2,'Captain America');
+INSERT INTO options (user_id,title_id,choice) VALUES (2,2,'The man from toronto');
