50ec68b4772afed83457d4045e5b4e4a3aa2b856
Zuge Gao <audreygaozu@hotmail.com>
2022-07-07 18:29:29 +0000

    before pull

--- routes/vote.js
+++ routes/vote.js
@@ -19,66 +19,67 @@ module.exports = (db) => {
     JOIN users ON users.id = options.user_id
     JOIN titles ON users.id = titles.user_id
     WHERE users.poll_key = $1;`, [poll_key.poll_key])
-    .then(data => {
+      .then(data => {
         const title = data.rows[0].title;
         let question = [];
-        let description=[];
+        let description = [];
         for (const i of data.rows) {
           question.push(i.choice);
           description.push(i.description);
         }
         console.log(title);
         console.log(question);
-        const info = {title:title, questions: question, description:description, poll_key:poll_key.poll_key};
+        const info = { title: title, questions: question, description: description, poll_key: poll_key.poll_key };
         console.log(info);
         //res.json({ info });
-        res.render("vote",info);
-    })
+        res.render("vote", info);
+      })
   });
 
   router.post("/:key", (req, res) => {
-    let poll_key = {poll_key:req.params.key};
+    console.log(req.body);
+    let poll_key = { poll_key: req.params.key };
     db.query(`SELECT users.email,titles.title
     FROM users
     JOIN titles ON users.id = titles.user_id
-    WHERE users.poll_key = $1;`,[poll_key.poll_key])
-    .then(data => {
-      const email = data.rows[0].email;
-      const title = data.rows[0].title;
-      console.log(email);
+    WHERE users.poll_key = $1;`, [poll_key.poll_key])
+      .then(data => {
+        const email = data.rows[0].email;
+        const title = data.rows[0].title;
+        console.log(email);
+        var transport = nodemailer.createTransport({
+          host: 'smtp.zoho.com',
+          port: 465,
+          secure: true, //ssl
+          auth: {
 
-      var transport = nodemailer.createTransport({
-        host: 'smtp.zoho.com',
-        port: 465,
-        secure: true, //ssl
-        auth: {
-
-            user:process.env.EMAIL,
-            pass:process.env.PASSWORD
-        }
-    });
-
-    var mailOptions = {
-        from: 'decisionmakerapp007@zohomail.com',
-        to: email,
-        subject: `your friends just voted on your poll`,
-        text: `
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+          }
+        });
+        var mailOptions = {
+          from: 'decisionmakerapp007@zohomail.com',
+          to: email,
+          subject: `your friends just voted on your poll`,
+          text: `
         Hello!
         your friend has just voted on your poll: ${title}. Click below to see the updated results.
         Admin Link: http://localhost:8080/api/result/${poll_key.poll_key}
         Share the link below to get more responses.
         Voting Link: http://localhost:8080/api/vote/${poll_key.poll_key}
         Thank you!`
-    };
+        };
 
-    transport.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-    })
+        transport.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+
+
+      })
 
 
   });
@@ -86,4 +87,6 @@ module.exports = (db) => {
 
   return router;
 };
+
+
 
